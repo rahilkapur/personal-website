03b4f28d2d6c6c975e4c12cc2c2603cf395546e5
Rahil Kapur <rahilkapur@outlook.com>
2024-08-06 17:19:52 -0500

    implemented lazy loading images

--- frontend/src/screens/SkillCard.jsx
+++ frontend/src/screens/SkillCard.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useCallback} from "react";
-import { Card, Image } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useAnimate } from "framer-motion";
 
 function SkillCard(props) {
@@ -16,7 +17,7 @@ function SkillCard(props) {
     return (
         <Card className="skill-card" ref={cardContent} style={{ backgroundColor: '#f8f9fa', color: '#000', borderRadius: '10px', padding: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', width: '180px', height: '220px', margin: '10px auto' }}>
             <div className="d-flex justify-content-center align-items-center" style={{ height: '120px' }}>
-                <Image src={props.imgpath} alt={props.imgalt} rounded style={{ height: '80px', width: '80px' }} />
+                <LazyLoadImage src={props.imgpath} alt={props.imgalt} rounded style={{ height: '80px', width: '80px' }} />
             </div>
             <h3 style={{ color: '#000', textAlign: 'center', margin: '10px 0' }}>{props.language}</h3>
         </Card>
